ca0a6f910101a033883e3eb687eb017629adb25a
s811068 <91081867+s811068@users.noreply.github.com>
2022-05-02 10:39:34 +0800

    Update triangle.c

--- triangle.c
+++ triangle.c
@@ -4,7 +4,7 @@ main () {
     scanf("%d", &n);
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n-i; j++) {
-                if(i % 2){
+                if(i % 2 == 0 ){
                    printf("*");
                 } else {
                  printf("+");
